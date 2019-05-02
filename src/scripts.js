5706fd78334dcee790df0bdf63dbcc65aef811a6
Hannah Frost <h.7frost@gmail.com>
2019-05-02 15:47:27 +0100

    Reset button resets currentTemp

--- src/scripts.js
+++ src/scripts.js
@@ -2,9 +2,19 @@ $(document).ready(function() {
   var thermostat = new Thermostat();
   $('#showTemp').text(thermostat.currentTemp);
   $('#PMS').text(thermostat.PMS);
-});
 
-$('#up').click(function() {
-  thermostat.up();
-  $('#showTemp').text(thermostat.currentTemp);
+  $('#up').click(function() {
+    thermostat.up();
+    $('#showTemp').text(thermostat.currentTemp);
+  });
+
+  $('#down').click(function() {
+    thermostat.down();
+    $('#showTemp').text(thermostat.currentTemp);
+  });
+
+  $('#reset').click(function() {
+    thermostat.reset();
+    $('#showTemp').text(thermostat.currentTemp);
+  });
 });
