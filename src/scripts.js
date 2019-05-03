bfa415116c36f4ea0b88b3bec9cbfd98811ac561
Hannah Frost <h.7frost@gmail.com>
2019-05-03 10:30:37 +0100

    Added ability to choose location

--- src/scripts.js
+++ src/scripts.js
@@ -29,8 +29,14 @@ $(document).ready(function() {
     $('#PMS').text(thermostat.PMS);
   });
 
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
     $('#weatherTemp').text(data.main.temp);
-    $('#location').text(data.name);
-  })
+  });
+
+  $('#location').change(function() {
+    var city = $('#location').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      $('#weatherTemp').text(data.main.temp);
+    });
+  });
 });
